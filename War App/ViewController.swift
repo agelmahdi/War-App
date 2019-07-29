7b18f10b5c93294d0c1998e6914b3ef400c452a5
Ahmed El-Mhadi <agelmahdi@gmail.com>
2019-07-29 23:06:10 +0200

    Initial Commit

---
+++ War App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  War App
+//
+//  Created by Ahmed El-Mahdi on 7/29/19.
+//  Copyright © 2019 Ahmed El-Mahdi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
