6009cdd77ded1abe51cbe10505e27a3595878bfc
Ahmed El-Mhadi <agelmahdi@gmail.com>
2019-07-30 22:38:03 +0200

    connect elements and functions

--- War App/ViewController.swift
+++ War App/ViewController.swift
@@ -10,11 +10,44 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var leftImageView: UIImageView!
+    
+    @IBOutlet weak var rightImageView: UIImageView!
+    
+    @IBOutlet weak var scoreLable: UILabel!
+    
+    @IBOutlet weak var cpuLable: UILabel!
+    
+    var leftScore=0
+    
+    var rightScore=0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    
+    @IBAction func dealBtn(_ sender: Any) {
+        let  leftnum = Int.random(in: 2...14)
+        let rightNum = Int.random(in: 2...14)
+        
+        leftImageView.image=UIImage(named: "card\(leftnum)")
+        rightImageView.image=UIImage(named: "card\(rightNum)")
+        if leftnum > rightNum {
+            leftScore = leftScore + 1
+            scoreLable.text = String(leftScore)
+        }
+        else if leftnum < rightNum
+        {
+            rightScore = rightScore + 1
+            cpuLable.text = String(rightScore)
+        }
+        else {
+            
+        }
+        
+    }
+    
 }
 
